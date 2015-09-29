12ac164d3ca13fd83c6c44ab3e0414f91d5155a9
Christoph Jansen <jansen@pando.io>
2015-09-29 12:28:33 +0200

    word level language model with word2vec input and one-hot output

---
+++ lstm_word2vec_language_model/helper.py
@@ -0,0 +1,161 @@
+__author__ = 'Christoph Jansen, HTW Berlin'
+
+import numpy as np
+import pickle
+import theano
+import theano.tensor as T
+import os
+from collections import Counter
+import gensim
+
+def acs(sentences, preserve_tokens, test_token=None, cv_token=None, test_size=.2, cv_size=.2):
+    counters = {}
+    for sentence in sentences:
+        for token in sentence:
+            if token in preserve_tokens:
+                counters[token] = counters.get(token, 0) + 1
+    sizes = {}
+    test_counters = {}
+    test_sents = set()
+    cv_counters = {}
+    cv_sents = set()
+    for i, sentence in enumerate(sentences):
+        for j, token in enumerate(sentence):
+            if counters.get(token, 0) * test_size > test_counters.get(token, 0):
+                test_counters[token] = test_counters.get(token, 0) + 1
+                test_sents.update([i])
+            elif (not i in test_sents) and (counters.get(token, 0) * cv_size > cv_counters.get(token, 0)):
+                cv_counters[token] = cv_counters.get(token, 0) + 1
+                cv_sents.update([i])
+    if test_token:
+        for i, sentence in enumerate(sentences):
+            if not i in test_sents:
+                continue
+            for j, token in enumerate(sentence):
+                if token == test_token:
+                    yield (sentence, j)
+    elif cv_token:
+        for i, sentence in enumerate(sentences):
+            if not i in cv_sents:
+                continue
+            for j, token in enumerate(sentence):
+                if token == cv_token:
+                    yield (sentence, j)
+    else:
+        for i, sentence in enumerate(sentences):
+            if (i in test_sents) or (i in cv_sents):
+                continue
+            yield sentence
+            
+def token_sequence(sentence, token_embeddings, w2v_embeddings):
+    tokens = ['S']+sentence+['E']
+    data = []
+    targets = []
+    for i in range(len(tokens)-1):
+        data.append(w2v_embeddings.token_to_vec(tokens[i]))
+        targets.append(token_embeddings.token_to_vec(tokens[i+1]))
+    return (theano_cast(data), theano_cast(targets))
+    
+def vec_to_index(one_hot):
+    for i, val in enumerate(one_hot):
+        if val > .99:
+            return i
+    raise Exception('No val greater than .99')
+
+def token_sequence_generator(sentences, token_embeddings, w2v_embeddings):
+    for i, sentence in enumerate(sentences):
+        yield token_sequence(sentence, token_embeddings, w2v_embeddings)
+            
+def theano_cast(M):
+    return np.asarray(M, dtype=theano.config.floatX)
+
+def save_states(objects, file_name, weights_dir):
+    p = os.path.join(weights_dir, file_name)
+    with open(p, 'wb') as f:
+        for obj in objects:
+            pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+    return file_name
+            
+def load_states(file_name, weights_dir, num_objects=17):
+    p = os.path.join(weights_dir, file_name)
+    loaded_objects = []
+    with open(p, 'rb') as f:
+        for i in range(num_objects):
+            loaded_objects.append(pickle.load(f))
+    return loaded_objects
+
+def save_errors(errors, file_name, weights_dir):
+    with open(os.path.join(weights_dir, file_name), 'wb') as f:
+        pickle.dump(errors, f, protocol=pickle.HIGHEST_PROTOCOL)
+    return file_name
+
+def load_errors(file_name, weights_dir, num_objects=17):
+    p = os.path.join(weights_dir, file_name)
+    with open(p, 'rb') as f:
+        errors = pickle.load(f)
+    return errors
+    
+def init_weights(shape):
+    return theano.shared(theano_cast(np.random.randn(shape[0], shape[1]) * 0.01))
+
+def normalization(token):
+    return ''.join(['D' if char.isdigit() else char for char in token]) 
+
+def init_zero_vec(size, one_hot_index=None):
+    vec = np.zeros(size, dtype=theano.config.floatX)
+    if one_hot_index:
+        vec[one_hot_index] = 1
+    return theano.shared(vec)
+    
+def t_softmax(vals, t=1.0):
+    exps = [np.exp(val/t) for val in vals]
+    s = sum(exps)
+    return [val/s for val in exps]
+    
+def count_tokens(sents, min_occurrence):
+    c = Counter()
+    for sentence in sents:
+        c.update(['S'] + sentence + ['E'])
+    
+    result = Counter()
+    for key, val in c.items():
+        if val < min_occurrence:
+            result.update({'U': val})
+        else:
+            result.update({key: val})
+    return result
+
+class TokenEmbeddings:
+    def __init__(self, sents, min_occurrence):
+        c = count_tokens(sents, min_occurrence)
+        sortable = list(c.keys())
+        sortable.sort()
+        self.token_to_index = {}
+        self.tokens = []
+        for i, token in enumerate(sortable):
+            self.token_to_index[token] = i
+            self.tokens.append(token)
+        self.num_tokens = len(self.tokens)
+        
+        print('num tokens: ', self.num_tokens)
+        print()
+    
+    def token_to_vec(self, token):
+        vec = np.zeros(self.num_tokens, theano.config.floatX)
+        vec[self.token_to_index.get(token, self.token_to_index['U'])] = 1
+        return vec
+        
+class Word2VecEmbeddings:
+    def __init__(self, file_location):
+        print('w2v - load embeddings...')
+        print()	
+        self.embeddings = gensim.models.Word2Vec.load(file_location)
+        self.embeddings_size = len(self.embeddings['the'])
+        print('w2v - word vector size: ', self.embeddings_size)
+        print()
+    
+    def token_to_vec(self, token):
+        try:
+            return self.embeddings[token]
+        except:
+            return self.embeddings['U']
--- prepare_data.py
+++ prepare_data.py
@@ -1,13 +1,22 @@
 import nltk
 import os
 import gensim
+from collections import Counter
 
-# dense word vector size for word2vec
+# dense word vector size for word2vec embeddings model
 word_vector_size = 128
+
+# dense word vector size for normalized word2vec embeddings model
+normalized_word_vector_size = 128
+
+# minimum occurrences of word in corpus for normalized embeddings
+# rare words are substituted by character 'U'
+min_occurrence = 20
 
 home_dir = os.path.expanduser('~')
 corpus_file = os.path.join(home_dir, 'brown_tagged.txt')
 embeddings_file = os.path.join(home_dir, 'embeddings.bin')
+normalized_embeddings_file = os.path.join(home_dir, 'normalized_embeddings.bin')
 
 nltk.download('brown')
 
@@ -19,10 +28,54 @@ with open(corpus_file, 'w') as f:
 		print(' '.join(['|'.join(tagged_word) for tagged_word in sentence]), file=f)
 
 print()
-print('generate word2vec model and save as file...')
+print('generate word2vec embeddings model and save as file...')
 
 with open(corpus_file) as f:
     sents = [[twp.split('|')[0].lower() for twp in line.split()] for line in f]
 	
 embeddings = gensim.models.Word2Vec(sents, size=word_vector_size, min_count=1)
 embeddings.save(embeddings_file)
+	
+print()
+print('generate normalized word2vec embeddings model and save as file...')
+
+def normalization(token):
+    return ''.join(['D' if char.isdigit() else char for char in token.lower()])
+	
+def count_tokens(sents, min_occurrence):
+    c = Counter()
+    for sentence in sents:
+        c.update(['S'] + sentence + ['E'])
+    
+    result = Counter()
+    for key, val in c.items():
+        if val < min_occurrence:
+            result.update({'U': val})
+        else:
+            result.update({key: val})
+    return result
+
+class TokenEmbeddings:
+    def __init__(self, sents, min_occurrence):
+        c = count_tokens(sents, min_occurrence)
+        sortable = list(c.keys())
+        sortable.sort()
+        self.token_to_index = {}
+        self.index_to_token = {}
+        for i, token in enumerate(sortable):
+            self.token_to_index[token] = i
+            self.index_to_token[i] = token
+        self.num_tokens = i + 1
+
+with open(corpus_file) as f:
+    sents = [['S'] + [normalization(twp.split('|')[0]) for twp in line.split()] + ['E'] for line in f]
+	
+token_embeddings = TokenEmbeddings(sents, min_occurrence)
+
+sents = [[token if token in token_embeddings.token_to_index else 'U' for token in s] for s in sents]
+	
+embeddings = gensim.models.Word2Vec(sents, size=word_vector_size, min_count=1)
+embeddings.save(normalized_embeddings_file)
+
+print()
+print('done')
